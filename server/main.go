e3653f85035f92944eea81fc4ff59678828d4ed1
Gabriel Duarte <gduarte93@aol.com>
2018-07-31 16:29:49 -0400

    add comment for when don't want to package with packr

--- server/main.go
+++ server/main.go
@@ -12,6 +12,11 @@ const port = ":4000"
 func main() {
 	box := packr.NewBox("../client")
 
+	// use http.Dir("../client") instead of box when you don't want to package with packr
+	//
+	// make sure to update Makefile location/build,
+	// since not using packr makes binary location important:
+	// must be in the server dir to recognize html/css/js files
 	http.Handle("/", http.FileServer(box))
 	http.HandleFunc("/getStatus", getStatusHandler)
 
